622f874ec37cfb15fcf128c08c650f28d9912279
bartekar <55534106+bartekar@users.noreply.github.com>
2023-04-21 00:56:14 +0200

    [ADD] more tests

--- tests/test_Board.py
+++ tests/test_Board.py
@@ -87,6 +87,12 @@ def test_undo_move_1():
     g.undo_move()
     assert True, 'undoing a move on an empty board...'
 
+# 0 1 2 3 4 5 6 7
+# . . . . . 1 . .
+# . . . . . 1 . .
+# . . . . . 1 . .
+# . . . . . 1 . .
+# . . . . . . . .
 @pytest.mark.simple
 def test_detect_win_at_0():
     g= Game()
@@ -98,6 +104,12 @@ def test_detect_win_at_0():
     assert g.detect_win_at(3, 5) == expected_symbol, 'vertical connection of 4'
     assert g.winner == 1
 
+# 0 1 2 3 4 5 6 7
+# . 2 2 2 2 . . .
+# . . . . . . . .
+# . . . . . . . .
+# . . . . . . . .
+# . . . . . . . .
 @pytest.mark.simple
 def test_win_at_1():
     g= Game()
@@ -106,11 +118,88 @@ def test_win_at_1():
     g.place_move(3, 2)
     g.place_move(4, 2)
     expected_symbol= 2
-    assert g.detect_win_at(1, 2) == expected_symbol, 'horizontal connection of 4'
-    assert g.detect_win_at(2, 2) == expected_symbol
-    assert g.detect_win_at(3, 2) == expected_symbol
-    assert g.detect_win_at(4, 2) == expected_symbol
-    assert g.detect_win_at(0, 2) == 0
-    assert g.detect_win_at(5, 2) == 0
+    assert g.detect_win_at(0, 1) == expected_symbol, 'horizontal connection of 4'
+    assert g.detect_win_at(0, 2) == expected_symbol
+    assert g.detect_win_at(0, 3) == expected_symbol
+    assert g.detect_win_at(0, 4) == expected_symbol
+    assert g.detect_win_at(0, 0) == 0
+    assert g.detect_win_at(0, 5) == 0
 
-# connection of 5, diagonal, on empty cells
+
+# 0 1 2 3 4 5 6 7
+# . 1 2 2 2 . . .
+# . . 1 2 1 . . .
+# . . . 1 2 . . .
+# . . . . 1 . . .
+# . . . . . . . .
+@pytest.mark.simple
+def test_win_at_2():
+    g= Game()
+    g.place_move(2, 2)
+    g.place_move(1, 1)
+    g.place_move(3, 2)
+    g.place_move(2, 1)
+    g.place_move(4, 2)
+    g.place_move(4, 1)
+    g.place_move(3, 2)
+    g.place_move(3, 1)
+    g.place_move(4, 2)
+    g.place_move(4, 1)
+    expected_symbol= 1
+    assert g.detect_win_at(0, 1) == expected_symbol, 'diagonal connection of 4'
+    assert g.detect_win_at(1, 2) == expected_symbol
+    assert g.detect_win_at(2, 3) == expected_symbol
+    assert g.detect_win_at(3, 4) == expected_symbol
+    assert g.detect_win_at(0, 2) == 0
+    assert g.detect_win_at(1, 4) == 0
+    assert g.detect_win_at(2, 4) == 0
+    assert g.detect_win_at(4, 5) == 0
+
+
+# 0 1 2 3 4 5 6 7
+# 1 1 1 2 2 . . .
+# 1 1 2 2 . . . .
+# 1 1 2 . . . . .
+# 2 2 . . . . . .
+# 2 . . . . . . .
+@pytest.mark.simple
+def test_win_at_2():
+    g= Game()
+    g.place_move(0, 1) # top left triangle
+    g.place_move(0, 1)
+    g.place_move(0, 1)
+    g.place_move(1, 1)
+    g.place_move(1, 1)
+    g.place_move(2, 1)
+
+    g.place_move(3, 2) # another diagonal
+    g.place_move(2, 2)
+    g.place_move(1, 1)
+    g.place_move(0, 2)
+
+    g.place_move(0, 2) # finishing diagonal
+    g.place_move(1, 2)
+    g.place_move(3, 2)
+    g.place_move(4, 2)
+
+    assert g.detect_win_at(4, 0) == 0, 'this lines has not won anything yet'
+    assert g.detect_win_at(3, 1) == 0
+    assert g.detect_win_at(2, 2) == 0
+    assert g.detect_win_at(1, 3) == 0
+    assert g.detect_win_at(0, 4) == 0
+    assert g.detect_win_at(1, 1) == 0
+
+    g.place_move(2, 2) # winning move
+
+    assert g.detect_win_at(4, 0) == 2, 'now there is a winning line with 5 symbols'
+    assert g.detect_win_at(3, 1) == 2
+    assert g.detect_win_at(2, 2) == 2
+    assert g.detect_win_at(1, 3) == 2
+    assert g.detect_win_at(0, 4) == 2
+    assert g.detect_win_at(1, 1) == 0
+
+
+
+
+
+
