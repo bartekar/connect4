eac0e912a557460f07173e3266773a2155631f76
bartekar <55534106+bartekar@users.noreply.github.com>
2023-05-11 00:04:20 +0200

    [CHG] whitespaces

--- Main.py
+++ Main.py
@@ -3,33 +3,33 @@ from Board import Game
 # todo: bei vollem brett schmiert das prgm ab
 
 def humanVSrandom():
-	game = Game()
-	print("VIER GEWINNT")
-	print(game)
-	print("Enter a valid number between 0 an 7 and collect 4 symbols")
-	
-	turn = 1
-	while not game.is_over():
-		if turn == 1:
-			move = int(input())
-			game.place_move(move, 1)
-			print(game)
-			turn = 2
-		else:
-			#game.do_random_move(2)
-			move, _ = game._do_suboptimal_search(turn, 2, 20)
-			#print(game)
-			game.place_move(move, 2)
-			print(game)
-			turn = 1
-			
-	if game.winner == None:
-		print("Draw")
-	else:
-		if game.winner == 1:
-			print("You win")
-		else:
-			print("Computer wins")
+    game = Game()
+    print("VIER GEWINNT")
+    print(game)
+    print("Enter a valid number between 0 an 7 and collect 4 symbols")
+    
+    turn = 1
+    while not game.is_over():
+        if turn == 1:
+            move = int(input())
+            game.place_move(move, 1)
+            print(game)
+            turn = 2
+        else:
+            #game.do_random_move(2)
+            move, _ = game._do_suboptimal_search(turn, 2, 20)
+            #print(game)
+            game.place_move(move, 2)
+            print(game)
+            turn = 1
+
+    if game.winner == None:
+        print("Draw")
+    else:
+        if game.winner == 1:
+            print("You win")
+        else:
+            print("Computer wins")
 
 if __name__ == "__main__":
-	humanVSrandom()
+    humanVSrandom()
